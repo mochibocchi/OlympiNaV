e1fd3e3f87a183db7f37767ff072d58cd98dab00
Timmy <116723040+mochibocchi@users.noreply.github.com>
2023-09-18 02:13:57 +1000

    Cleaned up code

--- app/src/main/java/com/example/olympinav/SavedTripsActivity.java
+++ app/src/main/java/com/example/olympinav/SavedTripsActivity.java
@@ -2,13 +2,9 @@ package com.example.olympinav;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 public class SavedTripsActivity extends AppCompatActivity {
 
@@ -17,11 +13,9 @@ public class SavedTripsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_trips);
 
-        // Find the button by its ID
-        ImageButton toastButton = findViewById(R.id.backButton);
+        ImageButton backButton = findViewById(R.id.backButton);
 
-        // Set an OnClickListener for the button
-        toastButton.setOnClickListener(new View.OnClickListener() {
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
