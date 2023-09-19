8710ef89427b03f9df97fc58bfb5c354acbf5fbd
Timmy <116723040+mochibocchi@users.noreply.github.com>
2023-09-19 22:53:23 +1000

    Add back button in activity_scan_qrcode.xml

--- app/src/main/java/com/example/olympinav/ScanQRCodeActivity.java
+++ app/src/main/java/com/example/olympinav/ScanQRCodeActivity.java
@@ -1,6 +1,7 @@
 package com.example.olympinav;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 
@@ -10,5 +11,9 @@ public class ScanQRCodeActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qrcode);
         setupActivity();
+        ImageButton backButton = findViewById(R.id.backButton);
+
+        // Press back button to exit out of activity
+        backButton.setOnClickListener(view -> finish());
     }
 }
