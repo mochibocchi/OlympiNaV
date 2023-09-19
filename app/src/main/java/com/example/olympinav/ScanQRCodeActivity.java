8a697750b5fc5d23d501d1c4b6998e3460661cc4
Timmy <116723040+mochibocchi@users.noreply.github.com>
2023-09-19 17:28:19 +1000

    Add general framework for activity_scan_qrcode.xml

--- app/src/main/java/com/example/olympinav/ScanQRCodeActivity.java
+++ app/src/main/java/com/example/olympinav/ScanQRCodeActivity.java
@@ -1,14 +1,14 @@
 package com.example.olympinav;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class ScanQRCodeActivity extends AppCompatActivity {
+import androidx.annotation.Nullable;
 
+public class ScanQRCodeActivity extends BaseActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qrcode);
+        setupActivity();
     }
 }
