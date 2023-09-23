1dcf81b4d6b23914b65d67595e78d6291cb5dd52
Timmy <116723040+mochibocchi@users.noreply.github.com>
2023-09-23 15:48:44 +1000

    Make activity_main.xml empty, add activity_main_with_added_event.xml and add navigation from activity_confirm_ticket.xml to activity_main.xml

--- app/src/main/java/com/example/olympinav/ConfirmTicketActivity.java
+++ app/src/main/java/com/example/olympinav/ConfirmTicketActivity.java
@@ -1,6 +1,8 @@
 package com.example.olympinav;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
@@ -10,5 +12,8 @@ public class ConfirmTicketActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_ticket);
         setupActivity();
+
+        Button btn = findViewById(R.id.okButton);
+        btn.setOnClickListener(v -> startActivity(new Intent(ConfirmTicketActivity.this, MainActivityWithAddedEvent.class)));
     }
 }
--- app/src/main/java/com/example/olympinav/MainActivity.java
+++ app/src/main/java/com/example/olympinav/MainActivity.java
@@ -16,7 +16,5 @@ public class MainActivity extends BaseActivity {
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ScanQRCodeActivity.class)));
-
-        findViewById(R.id.event).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, PlanTripActivity.class)));
     }
 }
