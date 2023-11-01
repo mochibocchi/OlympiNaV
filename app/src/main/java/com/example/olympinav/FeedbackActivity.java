881165adf311f96a00d49ff55ea991ab935ce0c7
Zac Gillerat <n10484698@qut.edu.au>
2023-11-01 18:23:52 +1000

    FeedbackActivity functionality.

--- app/src/main/java/com/example/olympinav/FeedbackActivity.java
+++ app/src/main/java/com/example/olympinav/FeedbackActivity.java
@@ -1,6 +1,11 @@
 package com.example.olympinav;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -10,5 +15,22 @@ public class FeedbackActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
         setupActivity();
+        setupViews();
+    }
+
+    private void setupViews() {
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+        // The comment edit text is not needed for validation purposes.
+        Button submitButton = findViewById(R.id.btnSubmit);
+
+        submitButton.setOnClickListener(v -> {
+            if (ratingBar.getRating() == 0) {
+                Toast.makeText(this, "Please select a star to provide us with a 1-5 rating", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Toast.makeText(this, "Thank you for your feedback", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(FeedbackActivity.this, MainActivity.class));
+        });
     }
 }
